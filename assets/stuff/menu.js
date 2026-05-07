4064e984f1f1ac404635dcc926b994277c07419b
elio-info <dario.andreu@gmail.com>
2026-05-06 20:03:25 -0400

    haciendo estructuras de pagina principal

--- assets/stuff/menu.js
+++ assets/stuff/menu.js
@@ -1,3 +1,87 @@
+const list_items=[
+    {name:`Caracteristicas de Pinar del Rio.`, desc:`Animate a un enfoque distinto de lo conocido...`, icon:'geo-fill', lnk:''},
+    {name:`Literatura certificada.`, desc:'Toda una referencia de documentos que justifican este legado.', icon:'home', lnk:''},
+    {name:`Tradiciones locales.`, desc:`Historias que debes de conocer. No todo es tan tan...`, icon:'toggles2', lnk:''},
+    {name:`Documentales.`, desc:`Recorre filmicamente la localidad monumental.`, icon:'collection', lnk:''},
+    {name:`Lugares patrimoniales.`, desc:`Lo que perdura en el tiempo...`, icon:'calendar3', lnk:''},
+    {name:`Proyectos socioculturales.`, desc:`El movimiento cultural  juega su papel`, icon:'tools', lnk:''}
+];
+
+
 function setMenu_Up(params) {
+    /**
+     * <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em" aria-hidden="true">
+              <use xlink:href="#bootstrap"></use>
+            </svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">
+                Featured title
+              </h3>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+     */
     
+}
+
+function setList_CenterArticles_Links(params) {
+    /**
+     * <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em" aria-hidden="true">
+              <use xlink:href="#bootstrap"></use>
+            </svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">
+                Featured title
+              </h3>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+              <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                Continue reading
+                <svg class="bi" aria-hidden="true">
+                  <use xlink:href="#chevron-right"></use>
+                </svg>
+              </a>
+            </div>
+        </div>
+     */
+    let center_link=``;
+    list_items.map((item)=>{
+        center_link +=`
+        <div class="col d-flex align-items-start py-3">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em" aria-hidden="true">
+              <use xlink:href="#${item.icon}"></use>
+            </svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">
+                ${item.name}
+              </h3>
+              <p>
+                ${item.desc}
+              </p>
+              <a href="#${item.lnk}" class="icon-link gap-1 icon-link-hover stretched-link">
+                Continue leyendo >>>
+                
+              </a>
+            </div>
+        </div>
+        `;
+    })
+    document.getElementById(params).innerHTML=center_link;
+}
+
+function set_Footer(params) {
+    let foot=` 
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item">            
+            <spam class="h4 px-1 text-body-secondary">Conservar es respetar</spam>
+          </li>
+        </ul>  
+        <p class="text-center text-body-secondary">&copy; 2025 Datamining, Inc</p>
+    `;
+    document.getElementById(params).innerHTML=foot;
 }
