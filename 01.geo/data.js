0c26eeb0a91eb1a076dc51227a5b931c1fd481c4
elio-info <dario.andreu@gmail.com>
2026-05-12 09:23:49 -0400

    cambios en : -inicio - geo

---
+++ 01.geo/data.js
@@ -0,0 +1,10 @@
+let data=[
+    {
+        place:``,title:`General`,
+        desc:`Pinar del Río es una provincia de Cuba, situada en el extremo occidental de la isla de Cuba y del archipiélago cubano. Limita al este con la provincia de Artemisa, al norte con el estrecho de Florida y al sur con el mar Caribe. Su capital es la ciudad de Pinar del Río.`
+    },
+    {
+        place:``,title:`Geografía`,
+        desc:`El centro está accidentado por la Sierra de los Órganos y el extremo este constituye la Sierra del Rosario con alturas superiores a los 600 m, ambas conforman la Cordillera de Guaniguanico, con una alineación suroeste-noroeste. Allí se localiza la Loma de Seboruco que con 671 m es la altitud máxima de la provincia.`
+    },
+]
--- assets/stuff/menu.js
+++ assets/stuff/menu.js
@@ -1,15 +1,12 @@
 const list_items=[
-    {name:`Caracteristicas de Pinar del Rio.`, desc:`Animate a un enfoque distinto de lo conocido...`, icon:'geo-fill', lnk:''},
-    {name:`Literatura certificada.`, desc:'Toda una referencia de documentos que justifican este legado.', icon:'home', lnk:''},
-    {name:`Tradiciones locales.`, desc:`Historias que debes de conocer. No todo es tan tan...`, icon:'toggles2', lnk:''},
-    {name:`Documentales.`, desc:`Recorre filmicamente la localidad monumental.`, icon:'collection', lnk:''},
-    {name:`Lugares patrimoniales.`, desc:`Lo que perdura en el tiempo...`, icon:'calendar3', lnk:''},
-    {name:`Proyectos socioculturales.`, desc:`El movimiento cultural  juega su papel`, icon:'tools', lnk:''}
+    {ttl:'Geo_Pinar',name:`Caracteristicas de Pinar del Rio.`, desc:`Animate a un enfoque distinto de lo conocido...`, icon:'geo-fill', lnk:'01.geo/geo'},
+    {ttl:'Litera_Tur',name:`Literatura certificada.`, desc:'Toda una referencia de documentos que justifican este legado.', icon:'home', lnk:''},
+    {ttl:'Tradi_Cult',name:`Tradiciones locales.`, desc:`Historias que debes de conocer. No todo es tan tan...`, icon:'toggles2', lnk:''},
+    {ttl:'Docu_Mental',name:`Documentales.`, desc:`Recorre filmicamente la localidad monumental.`, icon:'collection', lnk:''},
+    {ttl:'Patrimonios',name:`Lugares patrimoniales.`, desc:`Lo que perdura en el tiempo...`, icon:'calendar3', lnk:''},
+    {ttl:'Proyectos',name:`Proyectos socioculturales.`, desc:`El movimiento cultural  juega su papel`, icon:'tools', lnk:''}
 ];
-
-
-function setMenu_Up(params) {
-    /**
+/**
      * <div class="col d-flex align-items-start">
             <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em" aria-hidden="true">
               <use xlink:href="#bootstrap"></use>
@@ -24,7 +21,20 @@ function setMenu_Up(params) {
             </div>
           </div>
      */
+
+function setMenu_Up(params, posc) {
     
+   let menu_Up=`<ul class="nav nav-masthead"> ` ;
+   list_items.map((itm,indx)=>{
+      menu_Up +=`
+        <li class="nav-item">
+          <a href="${indx==posc?'#':itm.lnk}" class="nav-link ${indx==posc?'active':''}" ${indx==posc?'aria-current="page"':''}>${itm.ttl}</a>
+        </li>
+      `
+   });
+  menu_Up +=`</ul>`;
+  document.getElementById(params).innerHTML=menu_Up;
+   
 }
 
 function setList_CenterArticles_Links(params) {
@@ -63,7 +73,7 @@ function setList_CenterArticles_Links(params) {
               <p>
                 ${item.desc}
               </p>
-              <a href="#${item.lnk}" class="icon-link gap-1 icon-link-hover stretched-link">
+              <a href="${item.lnk}.patripi.html" class="icon-link gap-1 icon-link-hover stretched-link">
                 Continue leyendo >>>
                 
               </a>
