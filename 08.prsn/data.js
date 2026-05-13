7a7a16dc6d68a35ddf4d439b250ce40c3ee61274
elio-info <dario.andreu@gmail.com>
2026-05-13 18:13:45 -0400

    lajas

--- 08.prsn/data.js
+++ 08.prsn/data.js
@@ -1,17 +1,23 @@
 let data=[
     {
-        place:`Pinos`,title:`General`,
-        desc:`Pinar del Río es una provincia de Cuba, situada en el extremo occidental de la isla de Cuba y del archipiélago cubano. Limita al este con la provincia de Artemisa, al norte con el estrecho de Florida y al sur con el mar Caribe. Su capital es la ciudad de Pinar del Río.`
+        place:`Pinos`,title:`Pedro mPerras Pue`,grd:`MSc. Patrimonio y Salud`,
+        desc:`Destacado profesional en el extremo occidental de la isla de Cuba y del archipiélago cubano. Trabaja en la provincia de Pinar del Río.`
     },
     {
-        place:`Lugar`,title:`Geografía`,
+        place:`Lugar`,title:`Geografía`,grd:``,
         desc:`El centro está accidentado por la Sierra de los Órganos y el extremo este constituye la Sierra del Rosario con alturas superiores a los 600 m, ambas conforman la Cordillera de Guaniguanico, con una alineación suroeste-noroeste. Allí se localiza la Loma de Seboruco que con 671 m es la altitud máxima de la provincia.`
     },
 ]
 
 function create_Card(itm_data){
     let objCard=`
-    <div class="col-md-6">
+    <div class="col-md-4">
+
+        <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img rounded-circle" height="150" preserveAspectRatio="xMidYMid slice" role="img" width="200" xmlns="http://www.w3.org/2000/svg">
+            <title>${itm_data.place}</title>
+            <rect width="100%" height="100%" fill="#55595c"></rect>
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">${itm_data.place}</text>
+        </svg>
         <div class="row g-0 borderP rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
                 <h3 class="mb-0 fw-bold fs-4 text-body-emphasis"  >${itm_data.title}</h3>
@@ -21,11 +27,8 @@ function create_Card(itm_data){
                 </p>            
             </div>
             <div class="col-auto p-1 d-none d-lg-block">
-            <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img" height="250" preserveAspectRatio="xMidYMid slice" role="img" width="200" xmlns="http://www.w3.org/2000/svg">
-                <title>${itm_data.place}</title>
-                <rect width="100%" height="100%" fill="#55595c"></rect>
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">${itm_data.place}</text>
-            </svg>
+            
+                
             </div>
         </div>
     </div>`;
